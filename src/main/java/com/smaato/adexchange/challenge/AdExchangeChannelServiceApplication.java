fecafa123fd0405a5ec03c5db3ae3acab3feb54c
Chima Emmanuel Ezeamama <chimaemmanuel.ezeamama@gmail.com>
2023-03-11 19:35:41 +0100

    initialise test Challenge

---
+++ src/main/java/com/smaato/adexchange/challenge/AdExchangeChannelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smaato.adexchange.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdExchangeChannelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdExchangeChannelServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smaato/adexchange/challenge/AdExchangeChannelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smaato.adexchange.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdExchangeChannelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
